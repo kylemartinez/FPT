0e13b7ecf7dd743a6864286b832c22ae0b59a30c
kyle.martinez <kmartinez@convio.com>
2012-10-30 15:06:33 -0500

    higher order functions

---
+++ higher_order_procedure.rb
@@ -0,0 +1,19 @@
+class Filter
+  def initialize
+    @constraints = []
+  end
+  def constraint(&block)
+    @constraints << block
+  end
+  def to_proc
+    lambda { |e|
+      @constraints.all? { |fn| fn.call(e) } }
+  end
+end
+
+
+filter = Filter.new
+filter.constraint { |x| x > 10 }
+filter.constraint { |x| x.even? }
+filter.constraint { |x| x % 3 == 0 }
+p (8..24).select(&filter)
