115fab95895d660cbe188b9c2891f01ed1d6707f
kyle.martinez <kmartinez@convio.com>
2012-10-31 11:23:57 -0500

    timing functions

--- 02_min_mutable_state.rb
+++ 02_min_mutable_state.rb
@@ -1,16 +1,5 @@
 # minimize mutable data
 
-def naive_map(array)
-  array.each_with_object([]) {|e, obj| obj << yield(e)}
-end
-
-a = [1, 2, 3, 4]
-b = naive_map(a) {|x| x + 1}
-
-raise ArgumentError.new "this should not run" unless a == [1, 2, 3, 4]
-raise ArgumentError.new "this should not run" unless b == [2, 3, 4, 5]
-
-
 # adding elements to an array
 
 # << modifies its receiver
@@ -29,4 +18,45 @@ raise ArgumentError.new "this should not run" unless c == [1, 2, 3, 1, 2]
 raise ArgumentError.new "this should not run" unless b == [1, 2, 3, 1]
 raise ArgumentError.new "this should not run" unless a == [1, 2, 3, 1]
 
+
+def naive_map(array)
+  array.each_with_object([]) {|e, obj| obj << yield(e)}
+end
+
+a = [1, 2, 3, 4]
+b = naive_map(a) {|x| x + 1}
+
+raise ArgumentError.new "this should not run" unless a == [1, 2, 3, 4]
+raise ArgumentError.new "this should not run" unless b == [2, 3, 4, 5]
+
+
+def recursive_naive_map(array, &block)
+  if array.empty?
+    []
+  else
+    [ yield(array[0])] + recursive_naive_map(array[1..-1], &block)
+  end
+end
+
+def inject_naive_map(array, &block)
+  array.inject([]) {|base, e| [ yield(e)] + base}
+end
+
+a = [1, 2, 3, 4] * 20
+n = 100000
+
+#Naive Map Time Trial
+start = Time.now
+n.times { naive_map(a) {|x| x + 1} }
+puts "naive_map: #{Time.now - start}"
+
+#Recursive Naive Map
+start = Time.now
+n.times { recursive_naive_map(a) {|x| x + 1} }
+puts "recursive_naive_map: #{Time.now - start}"
+
+#Inject Naive Map
+start = Time.now
+n.times { inject_naive_map(a) {|x| x + 1} }
+puts "recursive_naive_map: #{Time.now - start}"
 
